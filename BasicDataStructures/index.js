c4bb8311b3de97d18a698fc49e59caffd3f50e8d
berkaynayman <berkaynayman4@gmail.com>
2021-08-15 18:28:45 +0300

    array

--- BasicDataStructures/index.js
+++ BasicDataStructures/index.js
@@ -91,3 +91,12 @@ function filteredArray(arr, elem) {
   }
 console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
 console.log(filteredArray([["amy", "beth", "sam"], ["dave", "sean", "peter"]], "peter"));
+
+/* ### Array Level ### */
+let myNestedArray = [
+    'level 1',                   /* myNestedArray[0]             */
+    ['level 2'],	               /* myNestedArray[1][0]          */
+    [['level 3','deep']],	       /* myNestedArray[2][0][0]       */
+    [[['level 4','deeper']]],    /* myNestedArray[3][0][0][0]    */
+    [[[['level 5','deepest']]]]  /* myNestedArray[4][0][0][0][0] */
+];
