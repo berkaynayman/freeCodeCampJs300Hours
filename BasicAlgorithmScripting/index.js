d3c9aa9c08c0f3e1a19f65b70edc225c791ecbde
berkaynayman <berkaynayman4@gmail.com>
2021-08-16 21:27:26 +0300

    BasicAlgorithmScripting

--- BasicAlgorithmScripting/index.js
+++ BasicAlgorithmScripting/index.js
@@ -4,7 +4,7 @@ function convertToF(celsius){
     fahrenheit = (celsius*9/5) + 32;
     return fahrenheit;
 }
-//console.log(convertToF(30));
+console.log(convertToF(30));
 /* ### Reverse String ### */
 function reverseString(str) {
     let strRe ="";
@@ -14,7 +14,7 @@ function reverseString(str) {
     str = strRe;
     return str;
 }
-//console.log(reverseString("hello"));
+console.log(reverseString("hello"));
 /* ### Faktoriyel ### */
 function factorialize(num) {
     let numRe = 1;
@@ -24,8 +24,8 @@ function factorialize(num) {
     num = numRe;
     return num;
 }  
-//console.log(factorialize(5));
-/* Cümle içerisinde en uzun kelime */
+console.log(factorialize(5));
+/* ### Cümle içerisinde en uzun kelime ### */
 function findLongestWordLength(str) {
   let words = str.split(' ');
   let maxLength = 0;
@@ -39,3 +39,19 @@ function findLongestWordLength(str) {
   return maxLength;
 }
 findLongestWordLength("Lorem kdnvks samf ppppp adovodsvm");
+/* ### Verilen iç içe dizede en büyük sayıları return etmek ### */
+function largestOfFour(arr) {
+  let BigNumbers = [];
+  for(let i=0;i<arr.length;i++){
+    arr[i].sort(function(a,b){return b - a});
+    BigNumbers.push(arr[i][0]);
+  }
+  return BigNumbers;
+}
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+/* ### kelime sonunu kontrol eden fonksiyon ### */
+function confirmEnding(str, target) {
+  return str.slice(str.length-target.length) == target ? true : false;
+}
+console.log(confirmEnding("Bastian", ""));
+console.log(confirmEnding("Selam dünyalı, biz dostuz", "dostuz"));
