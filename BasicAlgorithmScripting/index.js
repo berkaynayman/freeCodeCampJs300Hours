c95fd1b480208eb48567400da0fa0d84c8f67741
berkaynayman <berkaynayman4@gmail.com>
2021-08-28 22:56:24 +0300

    Basic Algorithm

--- BasicAlgorithmScripting/index.js
+++ BasicAlgorithmScripting/index.js
@@ -151,3 +151,13 @@ function mutation(arr){
 }
 mutation(["Mary", "Army"]);
 mutation(["voodoo", "no"])
+/* ### Chunky Monkey ### */
+function chunkArrayInGroups(arr, size) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    newArr.push(arr.slice(i, i + size));
+  }
+  return newArr;
+}
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2);
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
