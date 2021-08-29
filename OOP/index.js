2831fe76048033723c36542f59f693550a4a9d11
berkaynayman <berkaynayman4@gmail.com>
2021-08-29 23:38:51 +0300

    OOP

--- OOP/index.js
+++ OOP/index.js
@@ -34,3 +34,41 @@ function Dog_2(name, color){
     this.numLegs = 4;
 }
 let terrier = new Dog_2("terrier", "white");
+
+// Verify an Object's Constructor with instanceof
+terrier instanceof Dog_2; // true
+hound instanceof Dog_2; // false
+
+// Understand Own Properties
+// Dog() ın içindeki name, color, numLegs properties dir
+let ownProps = [];
+for(let ccc in terrier){
+    if(terrier.hasOwnProperty(ccc)){
+        ownProps.push(ccc);
+    }
+}
+console.log(ownProps);
+
+// Use Prototype Properties to Reduce Duplicate Code
+Dog_2.prototype.friends = "Shibiba";
+console.log(terrier.friends);
+
+// Iterate Over All Properties
+/*
+function Bird(name) {
+  this.name = name;  //own property
+}
+Bird.prototype.numLegs = 2; // prototype property
+*/
+// own property ve prototype property ayırmak
+let ownProps1 = [];
+let prototypeProps = [];
+for(let property in terrier){
+    if(terrier.hasOwnProperty(property)){
+        ownProps1.push(property);
+    }else{
+        prototypeProps.push(property);
+    }
+}
+console.log("ownProps1 - " + ownProps1);
+console.log("prototypeProps - " + prototypeProps);
