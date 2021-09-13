146d49dd18008af628edaf236c22d8754dc50674
berkaynayman <berkaynayman4@gmail.com>
2021-09-13 12:09:52 +0300

    reduce()

--- Function/index.js
+++ Function/index.js
@@ -190,3 +190,84 @@ let countedNames1 = names1.reduce((allNames, name) => {
     return allNames
 }, {})
 console.log(countedNames1);
+
+const products = [
+    {
+        name : "Pencil",
+        brand : "FaberCastel"
+    },
+    {
+        name : "Color-Pencil",
+        brand : "FaberCastel"
+    },
+    {
+        name : "Nokia X123DF",
+        brand : "Nokia"
+    },
+    {
+        name : "Headphone",
+        brand : "Ttec"
+    },
+    {
+        name : "Xperia XA1",
+        brand : "Sony"
+    }
+]
+// productsati ögelerimi brandlarına göre listelemek istiyorum
+let group = 'brand';
+const groupProducts = products.reduce((acc, product) => {
+    /*console.log(" -- acc ---");
+    console.log(acc);
+    console.log(" -- product.brand -- "+ product.brand );*/
+    let key = product[group];
+    if(!acc[key]){
+        acc[key] = []
+    }
+    acc[key].push(product);
+    return acc;
+}, {});
+console.log(groupProducts);
+
+const groupBy = (array, property) => {
+    return array.reduce((acc, product) => {
+        let key = product[property]
+        if(!acc[key]){
+            acc[key] = []
+        }
+        acc[key].push(product);
+        return acc
+    }, {})
+}
+console.log("********");
+console.log(groupBy(products, 'brand'));
+
+const posts = [
+    {
+        title : "Post 1",
+        tags : ["html", "css"]
+    },
+    {
+        title : "Post 2",
+        tags : ["html", "javascript", "json"]
+    },
+    {
+        title : "Post 3",
+        tags : ["java", "C#"]
+    }
+]
+
+// posts dizimdeki bütün taglari almak gerekiyor
+const allTags = posts.reduce((acc, posts) => [...acc, ...posts.tags], [] );
+console.log(allTags)
+
+// tekrar eden ögeler olmasın istiyor isek
+const result4 = allTags.reduce((acc, item) => {
+    if(!acc.includes(item)){
+        acc.push(item);
+    }
+    return acc;
+}, [])
+console.log(result4);
+
+// ama bunun için js nin bize sunduğu bir function var onu kullanabiliriz
+console.log([... new Set(allTags)]);
