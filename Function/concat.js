b54df3dd84f6f663fb78624557068db00e8c530e
berkaynayman <berkaynayman4@gmail.com>
2021-09-14 11:50:30 +0300

    concat.js

---
+++ Function/concat.js
@@ -0,0 +1,9 @@
+// ##### concat() - array birleşimi yapar
+const array1 = ['a','b','c'];
+const array2 = ['d','e','f'];
+
+const newArray = array1.concat(array2);
+console.log(newArray);
+
+const array3 = [['b','e'],'r'];
+console.log(array3.concat(['k','a','y']));
